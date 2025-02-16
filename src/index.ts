5bfc221f5f2522e2324000ca5ce8666bf46dfeb8
Logan Munsee <lmmunsee@student.fullsail.edu>
2025-02-16 00:33:50 -0500

    everything works needs some refractoring

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,41 @@
 import "@/styles/index.scss";
 import { Cards } from "./until";
 
+const cardFlip = document.querySelectorAll(".card__inner");
+const setAttemps: HTMLElement = document.querySelector(
+  "#attempts"
+) as HTMLElement;
+const gameStatus: HTMLElement = document.querySelector(
+  "#gameStatus"
+) as HTMLElement;
+const btn = document.querySelector("button");
+
+let firstCard: HTMLElement | null = null;
+let secondCard: HTMLElement | null = null;
+let isProcessing: boolean = false;
+let attemptCount: number = 3;
+let matchCount: number = 0;
+
+cardFlip.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (isProcessing || element.classList.contains("is-flipped")) return;
+
+    element.classList.add("is-flipped");
+
+    if (!firstCard) {
+      firstCard = element as HTMLElement;
+    } else if (!secondCard) {
+      secondCard = element as HTMLElement;
+      isProcessing = true;
+      matchCards();
+    }
+  });
+});
+
+btn?.addEventListener("click", () => {
+  startOver();
+});
+
 class Card {
   value: Cards;
   str: string;
@@ -22,7 +57,8 @@ const getRandomCard = (): Cards => {
 const displayCards = () => {
   let i = 0;
 
-  const cardDivs = document.querySelectorAll("div");
+  const cardDivs: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".card-value");
   const newCards: Card[] = [];
   while (i < 3) {
     newCards.push(new Card(getRandomCard()));
@@ -41,6 +77,76 @@ const displayCards = () => {
       cardDivs[j].innerText = `${element.value}`;
     }
   }
+};
+
+const matchCards = () => {
+  if (firstCard && secondCard) {
+    const firstCardValue = (
+      firstCard.querySelector(".card-value") as HTMLElement
+    )?.innerText;
+    const secondCardValue = (
+      secondCard.querySelector(".card-value") as HTMLElement
+    )?.innerText;
+
+    if (firstCardValue === secondCardValue) {
+      console.log("Match found!");
+      firstCard = null;
+      secondCard = null;
+      isProcessing = false;
+      matchCount++;
+      attemptCount--;
+      setAttemps.innerHTML = `Attempts left: ${attemptCount}`;
+      if (matchCount > 1 || attemptCount === 0) {
+        gameOver();
+      }
+    } else {
+      console.log("No match.");
+      attemptCount--;
+      setAttemps.innerHTML = `Attempts left: ${attemptCount}`;
+      if (attemptCount === 0) {
+        gameOver();
+      }
+      if (attemptCount > 0) {
+        setTimeout(() => {
+          firstCard?.classList.remove("is-flipped");
+          secondCard?.classList.remove("is-flipped");
+          console.log("flip");
+          firstCard = null;
+          secondCard = null;
+          isProcessing = false;
+        }, 1500);
+      }
+    }
+  }
+};
+
+const gameOver = () => {
+  isProcessing = true;
+
+  if (matchCount > 1) {
+    gameStatus.innerText = `You Win!`;
+  } else if (matchCount < 2) {
+    gameStatus.innerText = `You Lose!`;
+  }
+  cardFlip.forEach((element) => {
+    element.classList.add("is-flipped");
+  });
+};
+
+const startOver = () => {
+  attemptCount = 3;
+  matchCount = 0;
+  firstCard = null;
+  secondCard = null;
+  isProcessing = false;
+  gameStatus.innerText = "";
+  setAttemps.innerHTML = `Attempts left: ${attemptCount}`;
+  cardFlip.forEach((element) => {
+    element.classList.remove("is-flipped");
+  });
+  setTimeout(() => {
+    displayCards();
+  }, 400);
 };
 
 displayCards();
